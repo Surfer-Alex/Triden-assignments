037c86ade62ce7ab7ee400f8aa260b030bff1042
alex <126334654+Surfer-Alex@users.noreply.github.com>
2023-10-19 11:46:01 +0000

    Update 4 files

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,32 +1,32 @@
-'use client';
-import Logo from '@/components/Logo';
-import { useState } from 'react';
+"use client";
+import Logo from "@/components/Logo";
+import { useState } from "react";
 function Navigation() {
   const showMenuNav =
-    'flex flex-col  mt-[34.63px] xl:mt-[75.67px] mb-[45.22px] xl:mb-0';
-  const hideMenuNav = 'hidden';
+    "flex flex-col  mt-[34.63px] xl:mt-[75.67px] mb-[45.22px] xl:mb-0";
+  const hideMenuNav = "hidden";
   const [navOpen, setNavOpen] = useState(false);
 
   return (
     <nav className="relative flex flex-col items-center min-w-[280px] xl:min-w-[345px] bg-white xl:h-screen">
-      <div className=" z-20 relative nav-shadow w-full flex mt-[30.37px] xl:mt-[78.33px] items-center justify-center">
+      <div className=" z-20 relative nav-shadow xl:shadow-none w-full flex mt-[30.37px] xl:mt-[78.33px] items-center justify-center">
         <div
           onClick={() => setNavOpen((prev) => !prev)}
           className="space-y-2  absolute hover:animate-pulse  top-0 lg:h-[40.86px] flex flex-col justify-center	left-[26.38px] xl:hidden"
         >
           <span
             className={`block h-0.5 w-8  bg-black transform transition-transform duration-500 ${
-              navOpen ? 'rotate-45 translate-y-2.5' : ''
+              navOpen ? "rotate-45 translate-y-2.5" : ""
             }`}
           ></span>
           <span
             className={`block h-0.5 w-8 animate-none  bg-black transform transition-transform duration-500 ${
-              navOpen ? 'opacity-0' : ''
+              navOpen ? "opacity-0" : ""
             }`}
           ></span>
           <span
             className={`block h-0.5 w-8  bg-black transform transition-transform duration-500 ${
-              navOpen ? '-rotate-45 -translate-y-2.5' : ''
+              navOpen ? "-rotate-45 -translate-y-2.5" : ""
             }`}
           ></span>
         </div>
@@ -41,10 +41,10 @@ function Navigation() {
         } xl:flex xl:flex-col  xl:mt-[75.67px] xl:mb-0`}
       >
         {[
-          ['白頭翁的特性', '/'],
-          ['白頭翁的故事', '/'],
-          ['白頭翁的美照', '/'],
-          ['白頭翁的危機', '/'],
+          ["白頭翁的特性", "/"],
+          ["白頭翁的故事", "/"],
+          ["白頭翁的美照", "/"],
+          ["白頭翁的危機", "/"],
         ].map(([title, url], idx) => (
           <a
             key={idx}
